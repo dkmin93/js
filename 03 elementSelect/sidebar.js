99638a25c0500938833d7dc072cd5a3038c9ff9b
Andy <dkmin9392@gmail.com>
2025-03-11 17:55:02 +0900

    0311pm

---
+++ 03 elementSelect/sidebar.js
@@ -0,0 +1,32 @@
+
+var menuBtn = document.querySelector(".menuBtn");
+var sidenav = document.querySelector(".sidenav");
+
+menuBtn.addEventListener("click", function() {
+
+    var w = sidenav.style.width;
+  
+    // if(w == "0px" || w == "") {
+    //     sidenav.style.width = "200px";
+    // } else {
+    //     sidenav.style.width = "0px";
+    // }
+
+    if(window.innerWidth <= 767) { //모바일사이즈
+
+        if(w == "0px" || w == "") {
+            sidenav.style.width = "100%";
+        } else {
+            sidenav.style.width = "0px";
+        }
+
+    } else { //브라우저사이즈
+        if(w == "0px") {
+            sidenav.style.width = "200px";
+        } else {
+            sidenav.style.width = "0px";
+        }
+    }
+
+});
+
